157c44650fb73cc0e406ef47e12302dae6461635
alikanani572 <s357798@students.cdu.edu.au>
2023-05-24 23:32:09 +0930

    Addedd files for final Assignment AliZain's files finalised

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from app.models import Recipe, FoodItems, FoodWaste
+# Create your views here.
+def app(request):
+    return render(request, 'index.html')
+
+def index(request):
+    return render(request, 'index.html')
