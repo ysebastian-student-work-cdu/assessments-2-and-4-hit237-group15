a2ea94dffacf1be3641d024ec69bbfb917d7f39f
prashanna_bastola <prashanna.bastola@students.cdu.edu.au>
2023-05-26 23:02:10 +0930

    fooditem added

--- app/views.py
+++ app/views.py
@@ -64,5 +64,65 @@ def UPDATE_done_recipe(request, id):
     sel_recipe.save()
     messages.info(request, "Recipe updated")
 
-    
     return redirect('recipe')
+
+
+def food_item(request):
+    re = FoodItems.objects.all()
+    context = {
+        're' : re,
+    }
+    return render(request, 'fooditems.html', context)
+
+
+def ADD_fooditem(request):
+    if request.method == "POST":
+        itemID = request.POST.get('itemID')
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        expiryDate = request.POST.get('expiryDate')
+
+        re = FoodItems(
+            itemID = itemID,
+            name = name,
+            category = category,
+            expiryDate = expiryDate
+        )
+        re.save()
+        messages.info(request, "Food item Added")
+
+    return redirect('food_item')
+
+def DELETE_fooditem(request, id):
+    re = FoodItems.objects.filter(id = id)
+    re.delete()
+
+    context = {
+        're' : re,
+    }
+    messages.info(request, "Food item Deleted")
+
+    return redirect('food_item')
+
+
+
+def UPDATE_done_fooditem(request, id):
+    sel_fooditem = FoodItems.objects.get(id = id)
+    sel_fooditem.itemID = request.POST.get('itemID')
+    sel_fooditem.name = request.POST.get('name')
+    sel_fooditem.category = request.POST.get('category')
+    sel_fooditem.expiryDate = request.POST.get('expiryDate')
+    sel_fooditem.save()
+    messages.info(request, "Food item updated")
+
+    return redirect('food_item')
+
+def UPDATE_fooditem(request, id):
+    sel_fooditem = FoodItems.objects.get(id = id)
+    re = FoodItems.objects.all()
+    context = {
+        'sel_fooditem' : sel_fooditem,
+        're' : re,
+    }
+    
+    return render(request, 'fooditems.html', context)
