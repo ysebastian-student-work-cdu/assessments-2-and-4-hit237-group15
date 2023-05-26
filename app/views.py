e04cc9a7b6e2621d8f630db0187767e53955d232
NICKSON-PC\sangr <s357396@students.cdu.edu.au>
2023-05-26 21:38:11 +0930

    recipe render added

--- app/views.py
+++ app/views.py
@@ -9,5 +9,60 @@ def app(request):
 def index(request):
     return render(request, 'index.html')
 
-def index2(request):
-    return 1        #test function 
+def recipe(request):
+    re = Recipe.objects.all()
+    context = {
+        're' : re,
+    }
+    return render(request, 'recipe.html', context)
+
+def ADD_recipe(request):
+    if request.method == "POST":
+        recipeID = request.POST.get('recipeID')
+        name = request.POST.get('name')
+        ingredients = request.POST.get('ingredients')
+        instructions = request.POST.get('instructions')
+
+        re = Recipe(
+            recipeID = recipeID,
+            name = name,
+            ingredients = ingredients,
+            instructions = instructions
+        )
+        re.save()
+        messages.info(request, "Recipe Added")
+
+    return redirect('recipe')
+
+def DELETE_recipe(request, id):
+    re = Recipe.objects.filter(id = id)
+    re.delete()
+
+    context = {
+        're' : re,
+    }
+    messages.info(request, "Recipe Deleted")
+
+    return redirect('recipe')
+
+def UPDATE_recipe(request, id):
+    sel_recipe = Recipe.objects.get(id = id)
+    re = Recipe.objects.all()
+    context = {
+        'sel_recipe' : sel_recipe,
+        're' : re,
+    }
+    
+    return render(request, 'recipe.html', context)
+
+def UPDATE_done_recipe(request, id):
+    sel_recipe = Recipe.objects.get(id = id)
+    sel_recipe.recipeID = request.POST.get('recipeID')
+    sel_recipe.name = request.POST.get('name')
+    sel_recipe.ingredients = request.POST.get('ingredients')
+    sel_recipe.instructions = request.POST.get('instructions')
+    sel_recipe.save()
+    messages.info(request, "Recipe updated")
+
+    
+    return redirect('recipe')
