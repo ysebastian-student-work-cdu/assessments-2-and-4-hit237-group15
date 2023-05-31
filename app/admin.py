9b580d5b41ca74972c1b65e1bdc9a41e7a0b2052
NICKSON-PC\sangr <s357396@students.cdu.edu.au>
2023-05-31 19:29:08 +0930

    my part

--- app/admin.py
+++ app/admin.py
@@ -3,5 +3,3 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Audit_Record)
-admin.site.register(Food_Items)
--- app/models.py
+++ app/models.py
@@ -10,23 +10,3 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
-
-class Audit_Record(models.Model):
-    food_item  = models.TextField()
-    location = models.CharField(max_length=200)
-    audit_date = models.TextField()
-    waste_quantity = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-
-class Food_Items(models.Model):
-    name  = models.CharField(max_length=200)
-    category  = models.CharField(max_length=200)
-    quantity = models.TextField()
-    reason = models.TextField()
-    items = models.ManyToManyField(Audit_Record, related_name='unused_field')
-
-    def __str__(self):
-        return self.name
